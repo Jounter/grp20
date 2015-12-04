fcc3d0a34004d3cf6c6da392ca8121b9a023fcc7
Eligaos <david_barbosa13@hotmail.com>
2015-12-04 16:15:42 +0000

    change css and js to local

--- laravel/resources/views/gameLobby.blade.php
+++ laravel/resources/views/gameLobby.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en"  ng-app="lobby">
+<html lang="en" ng-app="lobby">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,26 +8,15 @@
     <meta name="description" content="">
     <meta name="author" content="">
     <title>Dashboard Template for Bootstrap</title>
-
     <!-- Bootstrap core CSS -->
     <link href="css/bootstrap.css" rel="stylesheet">
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <link href="css/ie10-viewport-bug-workaround.css" rel="stylesheet">
     <!-- Custom styles for this template -->
     <link href="css/lobby.css" rel="stylesheet">
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="js/ie-emulation-modes-warning.js"></script>
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-      <link rel="stylesheet" type="text/css" href="https://rawgit.com/rzajac/angularjs-slider/master/dist/rzslider.css">
-      <link rel="stylesheet" type="text/css" href="https://code.jquery.com/ui/1.11.4/themes/redmond/jquery-ui.css">
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.4.0/css/ngDialog.min.css">
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.4.0/css/ngDialog-theme-default.min.css">
-      <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.4.0/css/ngDialog-theme-default.min.css">
+      <link rel="stylesheet" type="text/css" href="css/rzslider.css">
+      <link rel="stylesheet" type="text/css" href="css/jquery-ui.css">
+      <link rel="stylesheet" type="text/css" href="css/ngDialog.min.css">
+      <link rel="stylesheet" type="text/css" href="css/ngDialog-theme-default.min.css">
+
 
   </head>
 
@@ -198,19 +187,15 @@
 </div>
      </div>
    </div>
-
       </div>
-
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <script src="http://code.jquery.com/jquery-2.1.4.js"></script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.7/angular.js"></script>
-    <script src="https://rawgit.com/rzajac/angularjs-slider/master/dist/rzslider.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/ng-dialog/0.4.0/js/ngDialog.min.js"></script>
+    <script src="js/jquery-2.1.4.js"></script>
+    <script src="js/angular.js"></script>
+    <script src="js/rzslider.js"></script>
+    <script src="js/ngDialog.min.js"></script>
     <script src="js/bootstrap.js"></script>
-     <script src="js/lobby.js"></script>
-    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="js/ie10-viewport-bug-workaround.js"></script>
+    <script src="js/lobby.js"></script>
   </body>
 </html>
